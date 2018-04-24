9c3c6795a4efd7e8cf30df542341f9592a7da698
Hikmawan <hikmawan1232@gmail.com>
2018-04-24 20:47:59 +0700

    signup activity

--- app/src/main/java/com/arbaini/getih/SignupActivity.java
+++ app/src/main/java/com/arbaini/getih/SignupActivity.java
@@ -7,12 +7,17 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText etNama,etEmail,etPass,etGolDar,etBB;
-    private String stNama,stEmail,stPass,stUmur,stGolDar,stBB;
+    private EditText etNama, etEmail, etPass, etGolDar, etBB;
+    private String stNama, stEmail, stPass, stUmur, stGolDar, stBB;
     private Button btnRegister;
+
+    private android.widget.Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,12 @@ public class SignupActivity extends AppCompatActivity {
         etEmail = findViewById(R.id.et_register_email);
         etPass = findViewById(R.id.et_register_password);
         etBB = findViewById(R.id.et_register_bb);
-        etGolDar = findViewById(R.id.et_register_goldarah);
 
         btnRegister = findViewById(R.id.btn_register);
+
+
+        spinner1 = (Spinner) findViewById(R.id.et_register_goldarah);
+        spinner1.setOnItemSelectedListener(new SpinnerSelectedListener());
 
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
@@ -36,51 +44,84 @@ public class SignupActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 stNama = etNama.getText().toString();
-                if(TextUtils.isEmpty(stNama)) {
+                if (TextUtils.isEmpty(stNama)) {
                     etNama.setError("Tidak boleh kosong");
                     return;
                 }
 
 
                 stEmail = etEmail.getText().toString();
-                if(TextUtils.isEmpty(stEmail)) {
+                if (TextUtils.isEmpty(stEmail)) {
                     etEmail.setError("Tidak boleh Kosong");
                     return;
                 }
 
                 stPass = etPass.getText().toString();
-                if(TextUtils.isEmpty(stPass)) {
+                if (TextUtils.isEmpty(stPass)) {
                     etPass.setError("Tidak boleh Kosong");
                     return;
                 }
 
                 stBB = etBB.getText().toString();
-                if(TextUtils.isEmpty(stBB)) {
+                if (TextUtils.isEmpty(stBB)) {
                     etBB.setError("Tidak boleh Kosong");
                     return;
                 }
 
-                stGolDar = etGolDar.getText().toString();
-                if(TextUtils.isEmpty(stGolDar)) {
-                    etGolDar.setError("Tidak boleh Kosong");
-                    return;
-                }
 
-
-
-                Intent intent = new Intent(SignupActivity.this,MainActivity.class);
+                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
                 Bundle extras = new Bundle();
-                extras.putString("NAMA",stNama);
-                extras.putString("EMAIL",stEmail);
-                extras.putString("PASS",stPass);
-                extras.putString("GOLDAR",stGolDar);
-                extras.putString("BB",stBB);
+                extras.putString("NAMA", stNama);
+                extras.putString("EMAIL", stEmail);
+                extras.putString("PASS", stPass);
+                extras.putString("GOLDAR", stGolDar);
+                extras.putString("BB", stBB);
                 intent.putExtras(extras);
                 startActivity(intent);
             }
         });
     }
 
+    public void setGoldarValue(String stGoldar) {
+
+
+        stGolDar = stGoldar;
+
+    }
+
+    public class SpinnerSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        //get strings of first item
+        String firstItem = String.valueOf(spinner1.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (firstItem.equals(String.valueOf(spinner1.getSelectedItem()))) {
+
+
+                setGoldarValue(parent.getItemAtPosition(pos).toString());
+
+               /* Toast.makeText(parent.getContext(),
+                        "You have selected : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+*/
+
+            } else {
+
+                setGoldarValue(parent.getItemAtPosition(pos).toString());
+                /*Toast.makeText(parent.getContext(),
+                        "You have selected : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+
+                */
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg) {
+
+        }
+
+    }
 
 
 }
